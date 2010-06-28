80343268a01ce3ef4ed6796bfc35b5436b8afa27
Premasagar <p@dharmafly.com>
2010-06-27 20:13:34 -0400

    console.js integrated into revolutionaries.js

--- revolutionaries.js
+++ revolutionaries.js
@@ -17,14 +17,99 @@
 
 var revolutionaries = (function(){
 
-    var monitorFreq = 250, // miiliseconds
+    var monitorFreq = 250, // milliseconds
         slugHistoryCache = [],
         slugCurrent = -1,
         window = this,
         jQuery = window.jQuery,
-        _ = window._,
         tmpl,
-        api;
+        api,
+        _; // debugging
+
+// **
+
+// EXTERNAL LIBRARIES
+
+/*!
+* Console
+*   github.com/premasagar/mishmash/tree/master/console/
+*
+*/
+
+_ = window._ = (function(){
+    var
+        window = this,
+        ua = window.navigator.userAgent,
+        console = window.console,
+        opera = window.opera,
+        debug;
+    
+    // Doesn't support console API
+    if (!console){
+        // Opera 
+        return (opera && opera.postError) ?
+             function(){
+                 var i, argLen, log = opera.postError, args = arguments, arg, subArgs, prop;
+                 log(args);
+                 
+                 argLen = args.length;
+	             for (i=0; i < argLen; i++){
+	                 arg = args[i];
+	                 if (typeof arg === 'object' && arg !== null){
+	                    subArgs = [];
+	                    for (prop in arg){
+	                        try {
+	                            if (arg.hasOwnProperty(prop)){
+	                                subArgs.push(prop + ': ' + arg[prop]);
+	                            }
+	                        }
+	                        catch(e){}
+	                    }
+	                    log('----subArgs: ' + subArgs);
+	                 }
+	             }
+             } :
+             function(){};
+    }
+    // Temporary for WebKit, while its console has a bug in calling debug directly or log.apply
+    else if (/webkit/i.test(ua)){	    
+        return function(){
+            var i, argLen, args = arguments, indent = '';
+            argLen = args.length;
+	        for (i=0; i < argLen; i++){
+	            if (typeof args[i] === 'object' && JSON && JSON.stringify){
+	                args[i] = JSON.stringify(args[i]);
+	            }
+		        console.log(indent + args[i]);
+                indent = '---- ';
+	        }
+        };
+    }
+    else {
+        debug = console.debug;
+        return debug ? // FF Firebug
+	        debug :
+	        function(){
+		        var i, argLen, log = console.log, args = arguments, indent = '';
+		        if (log){ // WebKit
+			        if (typeof log.apply === 'function'){
+				        log.apply(console, args);
+			        }
+			        else { // IE8
+				        argLen = args.length;
+				        for (i=0; i < argLen; i++){
+					        log(indent + args[i]);
+                            indent = '---- ';
+				        }
+			        }
+		        }
+	        };
+	}
+}());
+
+// end EXTERNAL LIBRARIES
+
+// **
     
     
     // CACHE
